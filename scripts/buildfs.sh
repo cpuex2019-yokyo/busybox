246bcb8f437724aef21317a903306315d11348ac
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-02-18 20:56:16 +0900

    remove copy of libs

--- scripts/buildfs.sh
+++ scripts/buildfs.sh
@@ -8,16 +8,16 @@
 set -e
 cd $(dirname "$0")
 
-IMAGE_PATH=../rootfs.img
 ROOT_PASSWORD=uouo
 
 # prepare rootfs image
-rm -f ${IMAGE_PATH=}
-dd if=/dev/zero of=${IMAGE_PATH} bs=1M count=300
-/sbin/mkfs.ext2 -j -F ${IMAGE_PATH}
+rm -f ../rootfs.img
+rm -f ../initramfs.cpio.gz
+dd if=/dev/zero of=../rootfs.img bs=1M count=300
+/sbin/mkfs.ext2 -j -F ../rootfs.img
 
 test -d mnt || mkdir mnt
-mount -o loop ${IMAGE_PATH} mnt
+mount -o loop ../rootfs.img mnt
 
 # set up in rootfs
 cd mnt
@@ -25,16 +25,18 @@ cp -r ../../_install/* .
 mkdir -p bin dev lib lib/modules proc sbin sys tmp usr usr/bin usr/sbin var/run var/log var/tmp etc
 ls
 cp ../../busybox bin/
-rsync -a --exclude ldscripts --exclude '*.la' --exclude '*.a' ${RISCV}/sysroot/lib/ lib/
-
 cp -r ../templates/etc/* etc/
+ln -s bin/busybox init
+cp ../templates/bin/ldd bin/ldd
+rsync -a --exclude ldscripts --exclude '*.la' --exclude '*.a' ${RISCV}/sysroot/lib/ lib/
+#cp ${RISCV}/sysroot/lib/libc.so.6 lib/libc.so.6
+rm lib/libnss*
+
+# set passwd
 hash=$(openssl passwd -1 -salt xyzzy ${ROOT_PASSWORD})
 sed -i'' "s:\*:${hash}:" etc/shadow
 chmod 600 etc/shadow
 
-#ln -s ../bin/busybox sbin/init
-ln -s bin/busybox init
-cp ../templates/bin/ldd bin/ldd
 
 mknod dev/console c 5 1
 mknod dev/ttyS0 c 4 64
